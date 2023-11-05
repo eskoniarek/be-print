213c9e799b21deef89e4e60da21cf6571401da64
eskoniarek <skoniarek93@gmail.com>
2023-11-05 19:23:48 +0100

    redis config

--- medusa-config.js
+++ medusa-config.js
@@ -96,14 +96,14 @@ const modules = {
   eventBus: {
     resolve: "@medusajs/event-bus-redis",
     options: {
-      redisUrl: process.env.EVENTS_REDIS_URL,
+      redisUrl: process.env.REDIS_URL,
 
     }
   },
   cacheService: {
     resolve: "@medusajs/cache-redis",
     options: {
-      redisUrl: process.env.CACHE_REDIS_URL,
+      redisUrl: process.env.REDIS_URL,
       ttl: 30,
     }
   },
